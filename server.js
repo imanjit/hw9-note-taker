3fc682767a8d5ddbfb75c53a6aa472b1e05723bd
Iman Bains <imanjitbains@yahoo.com>
2021-06-22 18:30:10 -0700

    Added dependencies and routes files

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const apiRoutes = require("./routes/api-routes");
+const htmlRoutes = require("./routes/html-routes");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -6,5 +8,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => console.log("App listening on PORT" + PORT));
