2d2d27db2ae755f5605baa165e8a30ce4caa15ac
Iman Bains <imanjitbains@yahoo.com>
2021-06-22 19:21:51 -0700

    Added html routes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
